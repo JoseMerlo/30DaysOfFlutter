b35c77bf3d025a2262a277f2ae8048d9a96cbfdc
Jose Merlo <josemerlo027@gmail.com>
2021-03-04 22:05:31 -0600

    Dart 2 complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,17 @@
+import 'package:flutter_catalogo/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to 30 days of Flutter"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
